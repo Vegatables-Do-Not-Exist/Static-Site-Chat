014a7f064c455d20a5900148c9b671496c59ed12
Vegatables-Do-Not-Exist <felixrie.25068@redlandsschools.net>
2025-06-01 18:02:17 -0700

    Update get-messages.js

--- netlify/functions/get-messages.js
+++ netlify/functions/get-messages.js
@@ -1,8 +1,37 @@
-// get-messages.js Netlify Function placeholder
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: "Get messages function placeholder"
-  };
-};
+const fetch = require("node-fetch");
 
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== "GET") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const airtableUrl = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_NAME}?sort%5B0%5D%5Bfield%5D=Timestamp&sort%5B0%5D%5Bdirection%5D=desc`;
+
+  try {
+    const res = await fetch(airtableUrl, {
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
+      },
+    });
+
+    if (!res.ok) {
+      const error = await res.text();
+      return {
+        statusCode: res.status,
+        body: JSON.stringify({ error }),
+      };
+    }
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
