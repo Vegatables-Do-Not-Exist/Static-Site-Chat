0b18dcc5bee6192326d04560e38db01faa9bb9ca
Vegatables-Do-Not-Exist <felixrie.25068@redlandsschools.net>
2025-06-01 17:59:59 -0700

    Update get-messages.js

--- netlify/functions/get-messages.js
+++ netlify/functions/get-messages.js
@@ -1,1 +1,8 @@
+// get-messages.js Netlify Function placeholder
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: "Get messages function placeholder"
+  };
+};
 
