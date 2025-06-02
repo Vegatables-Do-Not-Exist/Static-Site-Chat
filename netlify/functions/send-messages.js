398a7274f54a9563bdd148640969fbaa7e736700
Vegatables-Do-Not-Exist <felixrie.25068@redlandsschools.net>
2025-06-01 17:59:00 -0700

    Update send-messages.js

--- netlify/functions/send-messages.js
+++ netlify/functions/send-messages.js
@@ -1,1 +1,8 @@
+// send-message.js Netlify Function placeholder
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: "Send message function placeholder"
+  };
+};
 
